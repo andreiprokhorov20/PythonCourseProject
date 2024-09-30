f10df2d3cf12321fbfed80372db5e1ef90e9a378
andreiprokhorov20 <145938595+andreiprokhorov20@users.noreply.github.com>
2024-09-30 12:28:32 +0300

    Create task_5.py

---
+++ 2_week/task_5.py
@@ -0,0 +1,17 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/word-break/description
+"""
+
+class Solution:
+    def wordBreak(self, s, wordDict):
+        dp = [True] + [False] * len(s)
+
+        for i in range(1, len(s) + 1):
+            for w in wordDict:
+                start = i - len(w)
+                if start >= 0 and dp[start] and s[start:i] == w:
+                    dp[i] = True
+                    break
+        
+        return dp[-1]
