0e8b7484a72bf91002706a5ef7639549af622b87
Andrei <andrei.proxorov.20@mail.ru>
2024-10-13 18:42:40 +0300

    Changes in task_1

--- 3_week/task_1.py
+++ 3_week/task_1.py
@@ -10,7 +10,6 @@ class Solution(object):
         p2 = len(height) - 1
         ans = 0
         while p1 != p2:
-            #
             h = min(height[p1], height[p2])
             if ((p2 - p1) * h) > ans:
                 ans = (p2 - p1) * h
@@ -19,7 +18,3 @@ class Solution(object):
             else:
                 p2 += 1
         return ans
-        """
-        :type height: List[int]
-        :rtype: int
-        """
