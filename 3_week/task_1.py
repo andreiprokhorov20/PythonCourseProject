67b1845f683f3a14928bbd826c917518d44579ba
Andrei <andrei.proxorov.20@mail.ru>
2024-10-13 17:23:27 +0300

    Create 3_task_1

---
+++ 3_week/task_1.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problem-list/array/
+url: https://leetcode.com/problems/container-with-most-water/description
+"""
+
+
+class Solution(object):
+    def maxArea(self, height):
+        p1 = 0
+        p2 = len(height) - 1
+        ans = 0
+        while p1 != p2:
+            #
+            h = min(height[p1], height[p2])
+            if ((p2 - p1) * h) > ans:
+                ans = (p2 - p1) * h
+            if height[p1] < height[p2]:
+                p1 += 1
+            else:
+                p2 += 1
+        return ans
+        """
+        :type height: List[int]
+        :rtype: int
+        """
